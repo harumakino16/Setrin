f1c776e695c255164be0e5c78f7b017672fd2a6e
harumakiprog05 <harumakiprog05@gmail.com>
2024-05-01 01:39:39 +0900

    環境変数の再設定2

--- youtubeConfig.js
+++ youtubeConfig.js
@@ -1,7 +1,7 @@
 export const youtubeConfig = {
-    origin: "http://localhost:3000",
+    origin: process.env.NEXT_PUBLIC_ORIGIN,
     scope: "https://www.googleapis.com/auth/youtube",
-    redirectUri: "http://localhost:3000/setting",
+    redirectUri: process.env.NEXT_PUBLIC_REDIRECT_URI,
     youtubeApiKey: process.env.YOUTUBE_APIKEY,
     clientId: process.env.NEXT_PUBLIC_CLIENT_ID,
     clientSecret: process.env.CLIENT_SECRET,
