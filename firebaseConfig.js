e98f5e0dd0bcfdada2c1c57b121be64487094b64
harumakiprog05 <harumakiprog05@gmail.com>
2024-05-30 12:25:50 +0900

    firebaseの設定変更

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -1,15 +1,18 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
+import { getAnalytics } from "firebase/analytics";
 
 export const firebaseConfig = {
-  apiKey: "AIzaSyB9n_UaGmQ5fLH9T7gMp6od7WzgfyGtmcs",
-  authDomain: "setrin-9124a.firebaseapp.com",
-  projectId: "setrin-9124a",
-  storageBucket: "setrin-9124a.appspot.com",
-  messagingSenderId: "474078857325",
-  appId: "1:474078857325:web:4c3e82ee80c62d66351764",
-  measurementId: "G-WQ6L8VVTH3"
+  apiKey: "AIzaSyBpNvOh7IiyZzUv9Ysce0OijVlnXnWxFA0",
+  authDomain: "regal-muse-384522.firebaseapp.com",
+  projectId: "regal-muse-384522",
+  storageBucket: "regal-muse-384522.appspot.com",
+  messagingSenderId: "649488417421",
+  appId: "1:649488417421:web:2bdd25f3b5d0175defd8c4",
+  measurementId: "G-KR13S5GCRB"
 };
 
 export const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
+// export const analytics = getAnalytics(app);
+
