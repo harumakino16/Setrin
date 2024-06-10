3c61384d5d466e1fd0a92f2a98110dc3350e630f
harumakiprog05 <harumakiprog05@gmail.com>
2024-06-10 17:46:29 +0900

    firebaseのConfig設定を変更

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -3,16 +3,15 @@ import { getFirestore } from "firebase/firestore";
 import { getAnalytics } from "firebase/analytics";
 
 export const firebaseConfig = {
-  apiKey: "AIzaSyBpNvOh7IiyZzUv9Ysce0OijVlnXnWxFA0",
-  authDomain: "regal-muse-384522.firebaseapp.com",
-  projectId: "regal-muse-384522",
-  storageBucket: "regal-muse-384522.appspot.com",
-  messagingSenderId: "649488417421",
-  appId: "1:649488417421:web:2bdd25f3b5d0175defd8c4",
-  measurementId: "G-KR13S5GCRB"
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.FIREBASE_APP_ID,
+  measurementId: process.env.FIREBASE_MEASUREMENT_ID
 };
 
+console.log("firebaseConfig", process.env.FIREBASE_API_KEY);
 export const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
-// export const analytics = getAnalytics(app);
-
--- youtubeConfig.js
+++ youtubeConfig.js
@@ -3,6 +3,7 @@ export const youtubeConfig = {
     scope: "https://www.googleapis.com/auth/youtube",
     redirectUri: process.env.NEXT_PUBLIC_REDIRECT_URI,
     youtubeApiKey: process.env.YOUTUBE_APIKEY,
-    clientId: process.env.NEXT_PUBLIC_CLIENT_ID,
-    clientSecret: process.env.CLIENT_SECRET,
+    clientId: process.env.NEXT_PUBLIC_GOOGLE_CLOUD_CLIENT_ID,
+    clientSecret: process.env.GOOGLE_CLOUD_CLIENT_SECRET,
 };
+
