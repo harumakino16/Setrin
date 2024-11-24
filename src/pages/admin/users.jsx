6895cea7fb2d28ef006fcf0160814235120e6490
Takahiro <harumakino16@yahoo.co.jp>
2024-11-24 15:34:02 +0900

    総曲数取得の最適化

--- src/pages/admin/users.jsx
+++ src/pages/admin/users.jsx
@@ -1,9 +1,10 @@
 import withAdminAuth from '@/components/withAdminAuth';
 import { useEffect, useState } from 'react';
 import { db } from '../../../firebaseConfig';
-import { collection, getDocs, query, where } from 'firebase/firestore';
+import { collection, getDocs, getCountFromServer } from 'firebase/firestore';
 import { useMessage } from '@/context/MessageContext';
 import { getAuth, signInWithCustomToken } from 'firebase/auth';
+import Layout from '@/pages/layout';
 
 const ManageUsers = () => {
   const [users, setUsers] = useState([]);
@@ -45,16 +46,17 @@ const ManageUsers = () => {
     const fetchUsers = async () => {
       const usersCollection = collection(db, 'users');
       const usersSnapshot = await getDocs(usersCollection);
-      const usersList = await Promise.all(usersSnapshot.docs.map(async doc => {
-        const userData = doc.data();
-        const songsQuery = query(collection(db, 'users', doc.id, 'Songs'));
-        const songsSnapshot = await getDocs(songsQuery);
-        const totalSongs = songsSnapshot.size;
-        console.log('User ID:', doc.id);
-        console.log('Songs Query:', songsQuery);
-        console.log('Songs Snapshot Size:', songsSnapshot.size);
-        return { id: doc.id, ...userData, totalSongs };
-      }));
+      const usersList = await Promise.all(
+        usersSnapshot.docs.map(async (doc) => {
+          const userData = doc.data();
+          const songsCollection = collection(db, 'users', doc.id, 'Songs');
+          const songsSnapshot = await getCountFromServer(songsCollection);
+          const totalSongs = songsSnapshot.data().count;
+          console.log('User ID:', doc.id);
+          console.log('Total Songs:', totalSongs);
+          return { id: doc.id, ...userData, totalSongs };
+        })
+      );
       setUsers(usersList);
     };
 
@@ -62,38 +64,40 @@ const ManageUsers = () => {
   }, []);
 
   return (
-    <div className="p-5">
-      <h1 className="text-3xl font-bold mb-4">ユーザー管理</h1>
-      <table className="min-w-full bg-white">
-        <thead>
-          <tr>
-            <th className="py-2">ユーザーID</th>
-            <th className="py-2">メールアドレス</th>
-            <th className="py-2">表示名</th>
-            <th className="py-2">総曲数</th>
-            <th className="py-2">操作</th>
-          </tr>
-        </thead>
-        <tbody>
-          {users.map(user => (
-            <tr key={user.id}>
-              <td className="border px-4 py-2">{user.id}</td>
-              <td className="border px-4 py-2">{user.email}</td>
-              <td className="border px-4 py-2">{user.displayName}</td>
-              <td className="border px-4 py-2">{user.totalSongs}</td>
-              <td className="border px-4 py-2">
-                <button
-                  onClick={() => handleLoginAsUser(user.id)}
-                  className="bg-blue-500 text-white px-4 py-2 rounded"
-                >
-                  このユーザーとしてログイン
-                </button>
-              </td>
+    <Layout>
+      <div className="p-5">
+        <h1 className="text-3xl font-bold mb-4">ユーザー管理</h1>
+        <table className="min-w-full bg-white">
+          <thead>
+            <tr>
+              <th className="py-2">ユーザーID</th>
+              <th className="py-2">メールアドレス</th>
+              <th className="py-2">表示名</th>
+              <th className="py-2">総曲数</th>
+              <th className="py-2">操作</th>
             </tr>
-          ))}
-        </tbody>
-      </table>
-    </div>
+          </thead>
+          <tbody>
+            {users.map((user) => (
+              <tr key={user.id}>
+                <td className="border px-4 py-2">{user.id}</td>
+                <td className="border px-4 py-2">{user.email}</td>
+                <td className="border px-4 py-2">{user.displayName}</td>
+                <td className="border px-4 py-2">{user.totalSongs}</td>
+                <td className="border px-4 py-2">
+                  <button
+                    onClick={() => handleLoginAsUser(user.id)}
+                    className="bg-blue-500 text-white px-4 py-2 rounded"
+                  >
+                    このユーザーとしてログイン
+                  </button>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+    </Layout>
   );
 };
 
