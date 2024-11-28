ee4f1ce2058b448b908d70e045e331b4251c5fdc
Takahiro <harumakino16@yahoo.co.jp>
2024-11-28 16:48:22 +0900

    修正

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -5,8 +5,7 @@ const Footer = () => {
   return (
     <footer className="bg-gray-800 text-white text-center p-4 text-sm">
       <div className="flex flex-wrap justify-center gap-3">
-        <Link href="/about" className="text-white hover:underline">Setlinkとは</Link>
-        <Link href="/howto" className="text-white hover:underline">使い方</Link>
+        <Link href="/lp" className="text-white hover:underline">Setlinkとは</Link>
         <Link href="/termsuser" className="text-white hover:underline">利用規約</Link>
         <Link href="/tokusho" className="text-white hover:underline">特定商取引法に基づく表示</Link>
         <Link href="/privacypolicy" className="text-white hover:underline">プライバシーポリシー</Link>
--- src/pages/setlist/[id].jsx
+++ src/pages/setlist/[id].jsx
@@ -244,7 +244,7 @@ const SetlistDetail = () => {
                                                 className={`text-white font-bold py-2 px-4 rounded transition duration-300 ease-in-out ${!currentUser.youtubeRefreshToken || currentSongs.length === 0 ? ' bg-gray-400 hover:bg-gray-400 cursor-not-allowed disabled' : 'bg-red-500 hover:bg-red-700'}`}
                                             >
                                                 <FontAwesomeIcon icon={faYoutube} className="mr-2" />
-                                                YouTubeの再生リストに追加
+                                                YouTubeに再生リストを作成
                                             </button>
                                             <button
                                                 onClick={() => setIsColumnSettingsOpen(true)}
