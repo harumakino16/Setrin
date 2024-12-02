43ef360d83f945585ee74ec7d3ab127fa223135e
Takahiro <harumakino16@yahoo.co.jp>
2024-12-03 02:40:06 +0900

    再生リストからインポートし曲を登録するとき、フリガナは空白にする

--- src/pages/api/importPlaylist.js
+++ src/pages/api/importPlaylist.js
@@ -101,6 +101,7 @@ export default async function handler(req, res) {
                         genre: '',
                         singingCount: 0,
                         skillLevel: 0,
+                        furigana: ''
                     }));
 
                     return res.status(200).json({ 
