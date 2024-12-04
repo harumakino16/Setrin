9afd1c404ebdf63cea7c9c75748fd3933945ac88
Takahiro <harumakino16@yahoo.co.jp>
2024-12-04 17:31:39 +0900

    修正

--- src/pages/lp.jsx
+++ src/pages/lp.jsx
@@ -72,8 +72,8 @@ export default function LandingPage() {
               <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16m-7 6h7"></path>
             </svg>
           </button>
-          <nav className={`fixed top-0 right-0 h-full bg-white shadow-lg transform ${menuOpen ? 'translate-x-0' : 'translate-x-full'} z-50 transition-transform duration-300 ease-in-out md:static md:transform-none md:flex md:space-x-6`}>
-            <div className="flex justify-end p-4">
+          <nav className={`fixed top-0 right-0 h-full bg-white shadow-lg transform ${menuOpen ? 'translate-x-0' : 'translate-x-full'} z-50 transition-transform duration-300 ease-in-out md:shadow-none md:static md:transform-none md:flex md:items-center md:space-x-6`}>
+            <div className="flex justify-end p-4 md:hidden">
               <button onClick={closeMenu}>
                 <svg className="w-6 h-6 text-gray-500" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
                   <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M6 18L18 6M6 6l12 12"></path>
