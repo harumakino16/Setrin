b4eb05d217681e839d3532a9222180dc924c742b
Takahiro <harumakino16@yahoo.co.jp>
2024-12-15 00:40:27 +0900

    セットリストをまとめて削除する機能を追加

--- src/pages/setlist.jsx
+++ src/pages/setlist.jsx
@@ -16,9 +16,11 @@ export default function Setlist() {
   const [isOpen, setIsOpen] = useState(false);
   const [isEditOpen, setIsEditOpen] = useState(false);
   const [selectedSetlist, setSelectedSetlist] = useState(null);
+  const [selectedSetlists, setSelectedSetlists] = useState([]);
   const { setlists, loading } = useSetlists();
   const { theme } = useTheme();
   const router = useRouter();
+  const [isDeleteConfirmOpen, setIsDeleteConfirmOpen] = useState(false);
 
   const handleOpenModal = () => setIsOpen(true);
   const handleCloseModal = () => setIsOpen(false);
@@ -42,6 +44,35 @@ export default function Setlist() {
     }
   };
 
+  const handleSelectAll = (event) => {
+    if (event.target.checked) {
+      const allIds = setlists.map(setlist => setlist.id);
+      setSelectedSetlists(allIds);
+    } else {
+      setSelectedSetlists([]);
+    }
+  };
+
+  const handleSelect = (id) => {
+    setSelectedSetlists(prev =>
+      prev.includes(id) ? prev.filter(item => item !== id) : [...prev, id]
+    );
+  };
+
+  const handleOpenDeleteConfirmModal = () => setIsDeleteConfirmOpen(true);
+  const handleCloseDeleteConfirmModal = () => setIsDeleteConfirmOpen(false);
+
+  const handleDeleteSelected = async () => {
+    const deletePromises = selectedSetlists.map(id => {
+      const setlistRef = doc(db, 'users', currentUser.uid, 'Setlists', id);
+      return deleteDoc(setlistRef);
+    });
+    await Promise.all(deletePromises);
+    setSelectedSetlists([]);
+    setIsDeleteConfirmOpen(false);
+    setIsEditOpen(false);
+  };
+
   useEffect(() => {
     if (isEditOpen) {
       window.addEventListener('keydown', handleKeyDown);
@@ -60,12 +91,22 @@ export default function Setlist() {
         <div className="flex-grow p-4 md:p-8">
           <div className="flex flex-col md:flex-row justify-between items-center">
             <H1>セットリスト</H1>
-            <button onClick={handleOpenModal} className={`bg-customTheme-${theme}-primary hover:bg-customTheme-${theme}-accent text-white font-bold py-2 px-4 rounded inline-flex items-center mb-4 md:mb-0`}>
-              <svg className="w-4 h-4 mr-2" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M12 4v16m8-8H4" />
-              </svg>
-              新しいセットリストを追加
-            </button>
+            <div>
+              <button onClick={handleOpenModal} className={`bg-customTheme-${theme}-primary hover:bg-customTheme-${theme}-accent text-white font-bold py-2 px-4 rounded inline-flex items-center mb-4 md:mb-0`}>
+                <svg className="w-4 h-4 mr-2" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                  <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M12 4v16m8-8H4" />
+                </svg>
+                新しいセットリストを追加
+              </button>
+              {selectedSetlists.length > 0 && (
+                <button onClick={handleOpenDeleteConfirmModal} className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded ml-2 inline-flex items-center">
+                  <svg className="w-4 h-4 mr-2" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                    <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M6 18L18 6M6 6l12 12" />
+                  </svg>
+                  選択したセットリストを削除
+                </button>
+              )}
+            </div>
           </div>
           <div className="overflow-x-auto">
             {loading ? (
@@ -81,7 +122,15 @@ export default function Setlist() {
               <table className="min-w-full divide-y divide-gray-300 shadow-sm">
                 <thead className="bg-gray-50">
                   <tr>
-                    <th className="px-2 md:px-6 py-3 text-left text-xs md:text-sm font-semibold text-gray-600 uppercase tracking-wider">
+                    <th className="px-1 md:px-3 py-3 text-left max-w-1">
+                      <input
+                        type="checkbox"
+                        className='w-5 h-5'
+                        onChange={handleSelectAll}
+                        checked={selectedSetlists.length === setlists.length}
+                      />
+                    </th>
+                    <th className="px-1 md:px-3 py-3 text-left text-xs md:text-sm font-semibold text-gray-600 uppercase tracking-wider">
                       セットリスト名
                     </th>
                     <th className="px-2 md:px-6 py-3 text-left text-xs md:text-sm font-semibold text-gray-600 uppercase tracking-wider">
@@ -91,14 +140,29 @@ export default function Setlist() {
                       曲数
                     </th>
                     <th className="px-2 md:px-6 py-3 text-left text-xs md:text-sm font-semibold text-gray-600 uppercase tracking-wider">
-
                     </th>
                   </tr>
                 </thead>
                 <tbody className="bg-white divide-y divide-gray-200">
                   {setlists.map((setlist) => (
-                    <tr className='hover:cursor-pointer hover:bg-gray-100 transition-all' key={setlist.id} onClick={() => router.push(`/setlist/${setlist.id}`)} >
-                      <td className="px-2 md:px-6 py-4 whitespace-nowrap text-gray-900">
+                    <tr
+                      className='hover:cursor-pointer hover:bg-gray-100 transition-all'
+                      key={setlist.id}
+                      onClick={() => router.push(`/setlist/${setlist.id}`)}
+                    >
+                      <td className="px-1 md:px-3 py-4 whitespace-nowrap max-w-1">
+                        <input
+                          type="checkbox"
+                          className='w-5 h-5'
+                          checked={selectedSetlists.includes(setlist.id)}
+                          onChange={(e) => {
+                            e.stopPropagation();
+                            handleSelect(setlist.id);
+                          }}
+                          onClick={(e) => e.stopPropagation()}
+                        />
+                      </td>
+                      <td className="px-1 md:px-3 py-4 whitespace-nowrap text-gray-900">
                         {setlist.name}
                       </td>
                       <td className="px-2 md:px-6 py-4 whitespace-nowrap text-gray-900">
@@ -108,7 +172,14 @@ export default function Setlist() {
                         {setlist.songIds ? setlist.songIds.length : 0} 曲
                       </td>
                       <td className="px-2 md:px-6 py-4 whitespace-nowrap text-gray-900 text-right">
-                        <button onClick={(e) => { e.stopPropagation(); setSelectedSetlist(setlist); setIsEditOpen(true); }} className="text-red-500 hover:text-red-700">
+                        <button
+                          onClick={(e) => {
+                            e.stopPropagation();
+                            setSelectedSetlist(setlist);
+                            setIsEditOpen(true);
+                          }}
+                          className="text-red-500 hover:text-red-700"
+                        >
                           削除
                         </button>
                       </td>
@@ -131,6 +202,23 @@ export default function Setlist() {
                   キャンセル
                 </button>
                 <button onClick={handleDeleteSetlist} className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded">
+                  削除
+                </button>
+              </div>
+            </div>
+          </Modal>
+        )}
+        {isDeleteConfirmOpen && (
+          <Modal isOpen={isDeleteConfirmOpen} onClose={handleCloseDeleteConfirmModal}>
+            <div className="p-4">
+              <h2 className="text-lg mb-4">
+                {`${selectedSetlists.length} つのセットリストを削除しますか？`}
+              </h2>
+              <div className="flex justify-end mt-4">
+                <button onClick={handleCloseDeleteConfirmModal} className="bg-gray-500 hover:bg-gray-700 text-white font-bold py-2 px-4 rounded mr-2">
+                  キャンセル
+                </button>
+                <button onClick={handleDeleteSelected} className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded">
                   削除
                 </button>
               </div>
