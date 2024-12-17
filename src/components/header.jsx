1cdab8a6cfac1d8433ef4068580fe300e1782c28
Takahiro <harumakino16@yahoo.co.jp>
2024-12-18 01:59:03 +0900

    タイトル編集

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -11,18 +11,18 @@ import Head from 'next/head';
 const Header = ({ toggleSidebar }) => {
     const { theme } = useTheme();
     const router = useRouter();
-    const [publicPageColor, setPublicPageColor] = useState(null);
+    const [publicPageData, setPublicPageData] = useState([]);
 
     // Determine if the current page is a listener's page
     const isListenerPage = router.pathname.startsWith('/public');
-    const headerClassName = isListenerPage ? `bg-customTheme-${publicPageColor}-primary shadow-lg` : `bg-customTheme-${theme}-primary shadow-lg`;
+    const headerClassName = isListenerPage ? `bg-customTheme-${publicPageData.color}-primary shadow-lg` : `bg-customTheme-${theme}-primary shadow-lg`;
 
     useEffect(() => {
         if (isListenerPage) {
             const getPublicPage = async () => {
                 const collectionRef = doc(db, 'publicPages', router.query.id);
                 const snapshot = await getDoc(collectionRef);
-                setPublicPageColor(snapshot.data().color);
+                setPublicPageData(snapshot.data());
             }
             getPublicPage();
         }
@@ -42,11 +42,12 @@ const Header = ({ toggleSidebar }) => {
                         gtag('config', 'G-WQ6L8VVTH3');
                     `}
                 </script>
-                <title>SetLink</title>
+
+                {isListenerPage ? <title>{publicPageData.name} | SetLink</title> : <title>SetLink</title>}
                 <meta property="og:title" content="SetLink" />
                 <meta property="og:description" content="歌枠をもっと楽しく、もっと便利に" />
                 <meta name="description" content="SetlinkはVtuber向けに特化した歌枠サポートツールです。自分が歌える曲を簡単に管理し、公開リストを通じてリスナーからのリクエスト受付もスムーズに実現できます。これまで手間だったセットリスト作成が楽になり、リクエスト歌枠がもっと楽しくなる、歌枠をメインに活動するVtuberに最適なアプリです。" />
-                <meta name="keywords" content="Vtuber, セトリ, YouTube, 再生リスト, 管理ツール" />
+                <meta name="keywords" content="Vtuber, 歌枠, セトリ, YouTube, 再生リスト, 管理ツール" />
                 <meta property="og:image" content="https://setlink.vercel.app/images/bunner.png" />
                 <meta property="og:url" content="https://setlink.vercel.app/" />
                 <meta property="og:type" content="website" />
