f84ccc339424e5a1bedfe87b82fa0128d188be4f
Takahiro <harumakino16@yahoo.co.jp>
2024-12-24 06:46:34 +0900

    aタグの変更

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,5 +1,6 @@
 import { useTranslation } from 'next-i18next';
 import { serverSideTranslations } from 'next-i18next/serverSideTranslations';
+import Link from 'next/link';
 
 export default function Custom404() {
   const { t } = useTranslation('common');
@@ -7,7 +8,7 @@ export default function Custom404() {
     <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100">
       <h1 className="text-4xl font-bold text-gray-800">{t('404 - ページが見つかりません')}</h1>
       <p className="mt-4 text-gray-600">申し訳ありませんが、お探しのページは存在しません。</p>
-      <a href="/" className="mt-6 text-blue-500 hover:underline">ホームに戻る</a>
+      <Link href="/" className="mt-6 text-blue-500 hover:underline">ホームに戻る</Link>
     </div>
   );
 }
