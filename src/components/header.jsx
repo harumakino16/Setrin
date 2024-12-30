f6c8a8446ebec7e1d55155e420f37582bc23d7e3
Takahiro <harumakino16@yahoo.co.jp>
2024-12-31 08:55:44 +0900

    ・https://setlink.jpに置換 ・リダイレクト設定後

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -49,8 +49,8 @@ const Header = ({ toggleSidebar }) => {
                 <meta property="og:description" content="歌枠をもっと楽しく、もっと便利に" />
                 <meta name="description" content="SetlinkはVtuber向けに特化した歌枠サポートツールです。自分が歌える曲を簡単に管理し、公開リストを通じてリスナーからのリクエスト受付もスムーズに実現できます。これまで手間だったセットリスト作成が楽になり、リクエスト歌枠がもっと楽しくなる、歌枠をメインに活動するVtuberに最適なアプリです。" />
                 <meta name="keywords" content="Vtuber, 歌枠, セトリ, YouTube, 再生リスト, 管理ツール" />
-                <meta property="og:image" content="https://setlink.vercel.app/images/bunner.png" />
-                <meta property="og:url" content={`https://setlink.vercel.app${router.asPath}`} />
+                <meta property="og:image" content="https://setlink.jp/images/bunner.png" />
+                <meta property="og:url" content={`https://setlink.jp${router.asPath}`} />
                 <meta property="og:type" content="website" />
                 <meta property="og:site_name" content="Setlink" />
             </Head>
