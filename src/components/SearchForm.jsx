4ebf70ea5d61601f6932f81e83357aac42b3303c
harumakino16 <harumakino16@yahoo.co.jp>
2025-02-25 10:22:08 +0900

    SearchFormの翻訳完了

--- src/components/SearchForm.jsx
+++ src/components/SearchForm.jsx
@@ -3,10 +3,12 @@ import { useState, useEffect } from 'react';
 import { GoChevronDown, GoChevronUp } from 'react-icons/go'; // ReactIconのインポート
 import { useSongs } from '../context/SongsContext';
 import { convertKanaToHira, convertHiraToKana } from '../utils/stringUtils';
+import { useTranslation } from 'next-i18next'; // 翻訳フックをインポート
 
 const SearchForm = ({ currentUser, handleSearchResults, searchCriteria, setSearchCriteria, isRandomSetlist }) => {
     const [showAdvancedSearch, setShowAdvancedSearch] = useState(false);
     const { songs } = useSongs();
+    const { t } = useTranslation('common'); // 翻訳フックを初期化
 
     const searchSongs = async (searchCriteria) => {
         let songsData = songs;
@@ -108,17 +110,17 @@ const SearchForm = ({ currentUser, handleSearchResults, searchCriteria, setSearc
         <div>
             <div className="flex flex-col gap-8 mb-8">
                 <div className="">
-                    <label className="whitespace-nowrap">フリーキーワード:</label>
+                    <label className="whitespace-nowrap">{t('freeKeyword')}:</label>
                     <input 
                         type="text" 
                         className="border p-2 rounded w-full h-14" 
-                        placeholder="キーワードで検索" 
+                        placeholder={t('searchByKeyword')} 
                         value={searchCriteria.freeKeyword || ''}
                         onChange={(e) => handleCriteriaChange('freeKeyword', e.target.value)}
                     />
                 </div>
                 <div onClick={() => setShowAdvancedSearch(!showAdvancedSearch)} className="text-gray-500 py-2 px-4 cursor-pointer flex items-center">
-                    <span className="text-gray-500">{isRandomSetlist ? '絞り込み' : '詳細検索'}</span> 
+                    <span className="text-gray-500">{isRandomSetlist ? t('filter') : t('advancedSearch')}</span> 
                     <span className="text-gray-500">
                         {showAdvancedSearch ? <GoChevronUp size={20} /> : <GoChevronDown size={20} />}
                     </span>
@@ -127,20 +129,20 @@ const SearchForm = ({ currentUser, handleSearchResults, searchCriteria, setSearc
                     <div className="flex flex-wrap gap-8">
                         <div className="w-full">
                             <div className="p-4 bg-white shadow rounded-lg">
-                                <h3 className="font-bold mb-4">絞り込み</h3>
+                                <h3 className="font-bold mb-4">{t('filter')}</h3>
                                 <div className="flex flex-wrap gap-4">
                                     <div className="flex flex-col">
-                                        <label className="whitespace-nowrap">歌唱回数:</label>
+                                        <label className="whitespace-nowrap">{t('singingCount')}:</label>
                                         <input type="number" className="border p-2 rounded mb-4" value={searchCriteria.maxSung} onChange={(e) => handleCriteriaChange('maxSung', parseInt(e.target.value, 10))} />
                                         <div className="flex gap-2 radio02 mb-4">
                                             <input type="radio" id="maxSungOptionBelow" name="maxSungOption" value="以下" checked={searchCriteria.maxSungOption === '以下'} onChange={(e) => handleCriteriaChange('maxSungOption', e.target.value)} />
-                                            <label htmlFor="maxSungOptionBelow">以下</label>
+                                            <label htmlFor="maxSungOptionBelow">{t('orLess')}</label>
                                             <input type="radio" id="maxSungOptionAbove" name="maxSungOption" value="以上" checked={searchCriteria.maxSungOption === '以上'} onChange={(e) => handleCriteriaChange('maxSungOption', e.target.value)} />
-                                            <label htmlFor="maxSungOptionAbove">以上</label>
+                                            <label htmlFor="maxSungOptionAbove">{t('orMore')}</label>
                                         </div>
                                     </div>
                                     <div className="flex flex-col">
-                                        <label className="whitespace-nowrap">タグ:</label>
+                                        <label className="whitespace-nowrap">{t('tag')}:</label>
                                         <input 
                                             type="text" 
                                             className="border p-2 rounded mb-4" 
@@ -151,32 +153,32 @@ const SearchForm = ({ currentUser, handleSearchResults, searchCriteria, setSearc
                                                     handleCriteriaChange('tag', e.target.value);
                                                 }
                                             }} 
-                                            placeholder="カンマ区切りで最大5つまで" 
+                                            placeholder={t('commaSeparatedMaxFiveTags')} 
                                         />
                                         {searchCriteria.tag.split(',').filter(tag => tag.trim() !== '').length > 5 && (
-                                            <span className="text-red-500 text-sm">タグは最大5つまでです。</span>
+                                            <span className="text-red-500 text-sm">{t('maxFiveTags')}</span>
                                         )}
                                     </div>
                                     <div className="flex flex-col">
-                                        <label className="whitespace-nowrap">アーティスト名:</label>
+                                        <label className="whitespace-nowrap">{t('artist')}:</label>
                                         <input type="text" className="border p-2 rounded mb-4" value={searchCriteria.artist} onChange={(e) => handleCriteriaChange('artist', e.target.value)} />
                                     </div>
                                     <div className="flex flex-col">
-                                        <label className="whitespace-nowrap">ジャンル:</label>
+                                        <label className="whitespace-nowrap">{t('genre')}:</label>
                                         <input type="text" className="border p-2 rounded mb-4" value={searchCriteria.genre} onChange={(e) => handleCriteriaChange('genre', e.target.value)} />
                                     </div>
                                     <div className="flex flex-col">
-                                        <label className="whitespace-nowrap">熟練度:</label>
+                                        <label className="whitespace-nowrap">{t('skillLevel')}:</label>
                                         <input type="number" className="border p-2 rounded mb-4" value={searchCriteria.skillLevel} onChange={(e) => handleCriteriaChange('skillLevel', parseInt(e.target.value, 10))} />
                                         <div className="flex gap-2 radio02 mb-4">
                                             <input type="radio" id="skillLevelOptionBelow" name="skillLevelOption" value="以下" checked={searchCriteria.skillLevelOption === '以下'} onChange={(e) => handleCriteriaChange('skillLevelOption', e.target.value)} />
-                                            <label htmlFor="skillLevelOptionBelow">以下</label>
+                                            <label htmlFor="skillLevelOptionBelow">{t('orLess')}</label>
                                             <input type="radio" id="skillLevelOptionAbove" name="skillLevelOption" value="以上" checked={searchCriteria.skillLevelOption === '以上'} onChange={(e) => handleCriteriaChange('skillLevelOption', e.target.value)} />
-                                            <label htmlFor="skillLevelOptionAbove">以上</label>
+                                            <label htmlFor="skillLevelOptionAbove">{t('orMore')}</label>
                                         </div>
                                     </div>
                                     <div className="flex flex-col">
-                                        <label className="whitespace-nowrap">備考:</label>
+                                        <label className="whitespace-nowrap">{t('note')}:</label>
                                         <input 
                                             type="text" 
                                             className="border p-2 rounded mb-4" 
@@ -185,7 +187,7 @@ const SearchForm = ({ currentUser, handleSearchResults, searchCriteria, setSearc
                                         />
                                     </div>
                                     <div className="flex flex-col">
-                                        <label className="whitespace-nowrap">メモ:</label>
+                                        <label className="whitespace-nowrap">{t('memo')}:</label>
                                         <input 
                                             type="text" 
                                             className="border p-2 rounded mb-4" 
@@ -198,26 +200,26 @@ const SearchForm = ({ currentUser, handleSearchResults, searchCriteria, setSearc
                         </div>
                         <div className="w-full">
                             <div className="p-4 bg-white shadow rounded-lg">
-                                <h3 className="font-bold mb-4">除外</h3>
+                                <h3 className="font-bold mb-4">{t('exclude')}</h3>
                                 <div className="flex flex-wrap gap-4">
                                     <div className="flex flex-col">
-                                        <label className="whitespace-nowrap">除外タグ:</label>
+                                        <label className="whitespace-nowrap">{t('excludeTags')}:</label>
                                         <input 
                                             type="text" 
                                             className="border p-2 rounded mb-4" 
                                             value={searchCriteria.excludedTags} 
                                             onChange={(e) => handleCriteriaChange('excludedTags', e.target.value)} 
-                                            placeholder="カンマ区切りでタグを除外"
+                                            placeholder={t('commaSeparatedExcludeTags')}
                                         />
                                     </div>
                                     <div className="flex flex-col">
-                                        <label className="whitespace-nowrap">除外ジャンル:</label>
+                                        <label className="whitespace-nowrap">{t('excludeGenres')}:</label>
                                         <input 
                                             type="text" 
                                             className="border p-2 rounded mb-4" 
                                             value={searchCriteria.excludedGenres} 
                                             onChange={(e) => handleCriteriaChange('excludedGenres', e.target.value)} 
-                                            placeholder="カンマ区切りでジャンルを除外"
+                                            placeholder={t('commaSeparatedExcludeGenres')}
                                         />
                                     </div>
                                 </div>
